3215cb01318602049583a74f49d0cb8f0ec4f925
Escanor1986 <lionel.zovi@gmail.com>
2023-03-04 15:24:52 +0100

    beginning with the back aiming for fullstack

---
+++ front/src/NotFound.js
@@ -0,0 +1,15 @@
+import "./App.scss";
+
+function NotFound() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a className="App-link" href="/home" rel="noopener noreferrer">
+          Oups... page non trouvée ! Return to Home
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default NotFound;
